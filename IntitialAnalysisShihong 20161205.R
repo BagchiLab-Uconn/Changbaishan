fcdae79ad2408b4f2d2d644a8fe48ae790875179
shihongjia <shihong_jia@hotmail.com>
2016-12-12 13:52:44 -0600

    Updated initial analysis

--- IntitialAnalysisShihong 20161205.R
+++ IntitialAnalysisShihong 20161205.R
@@ -92,6 +92,7 @@ summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
 
 ## plot the figure
 # Standard error of the mean
+<<<<<<< Updated upstream
 
 # The errorbars overlapped, so use position_dodge to move them horizontally
 pd <- position_dodge(0.2) # move them .1 to the left and righ
@@ -107,6 +108,23 @@ summary(dat.15b)
 # height16a dataset
 summary(dat.16a)
 
+=======
+
+# The errorbars overlapped, so use position_dodge to move them horizontally
+pd <- position_dodge(0.2) # move them .1 to the left and righ
+
+
+################
+### survival ###
+################
+
+# remove the NAs
+# height15b dataset
+summary(dat.15b)
+# height16a dataset
+summary(dat.16a)
+
+>>>>>>> Stashed changes
 ## For the 15b dataset
 # initial status for the 15b
 dat.15b$surv0 <- as.numeric(!is.na(dat.15b$height15b))
@@ -134,7 +152,10 @@ ggplot(s1.cal, aes(x=treatment, y=s1, colour=as.factor(exclosure))) +
 
 # survival rate in the 16b census
 summary(dat.15b.surv$s2)
+<<<<<<< Updated upstream
 which.max(dat.15b.surv$s2)
+=======
+>>>>>>> Stashed changes
 s2.cal <- summarySE(dat.15b.surv,measurevar="s2", groupvars=c("site","exclosure","treatment"))
 #View(s2.cal)
 summary(s2.cal)
@@ -189,6 +210,44 @@ ggplot(ab2.cal, aes(x=treatment, y=ab2, colour=as.factor(exclosure))) +
   geom_line(position=pd) +
   geom_point(position=pd) +
   facet_wrap("site")
+<<<<<<< Updated upstream
+
+# Use 95% confidence interval instead of SEM
+ggplot(ab1.cal, aes(x=treatment, y=ab1, colour=as.factor(exclosure))) + 
+  geom_errorbar(aes(ymin=ab1-ci, ymax=ab1+ci), width=.1,position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd) +
+  facet_wrap("site")
+
+### end of the abundance
+
+#########################
+### number of species ###
+#########################
+
+# 16a
+dat.16a$no.species <-as.numeric(dat.16a$species)
+dat.16a.spec  <- aggregate(no.species~ treatment + exclosure + site + sub.code, data=dat.16a, FUN=function(x)length(unique(x)))
+#View(dat.16a.spec)
+# 16b
+dat.16b$no.species <-as.numeric(dat.16b$species)
+dat.16b.spec  <- aggregate(no.species~ treatment + exclosure + site + sub.code, data=dat.16b, FUN=function(x)length(unique(x)))
+#View(dat.16b.spec)
+
+spec1.cal <- summarySE(dat.16a.spec,measurevar="no.species", groupvars=c("site","exclosure","treatment"))
+spec2.cal <- summarySE(dat.16b.spec,measurevar="no.species", groupvars=c("site","exclosure","treatment"))
+
+# 16a 
+ggplot(spec1.cal, aes(x=treatment, y=no.species, colour=as.factor(exclosure))) + 
+  geom_errorbar(aes(ymin=no.species-se, ymax=no.species+se), width=.1,position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd) +
+  facet_wrap("site")
+
+# 16b
+ggplot(spec2.cal, aes(x=treatment, y=no.species, colour=as.factor(exclosure))) + 
+  geom_errorbar(aes(ymin=no.species-se, ymax=no.species+se), width=.1,position=pd) +
+=======
 
 # Use 95% confidence interval instead of SEM
 ggplot(ab1.cal, aes(x=treatment, y=ab1, colour=as.factor(exclosure))) + 
@@ -281,3 +340,131 @@ ggplot(heig1.2.cal, aes(x=treatment, y=heig2, colour=as.factor(exclosure))) +
   geom_line(position=pd) +
   geom_point(position=pd) +
   facet_wrap("site")
+
+########################
+### using glms function
+## incorporating the species effect
+
+# get mean survival rate for the quadarts for each treatment in different exclosure treatments
+# within each of the three sites
+dat.15b.surv  <- aggregate(cbind(surv0,surv1,surv2)~ treatment + exclosure + site + sub.code + species, data=dat.15b, FUN=sum)
+
+dat.15b.surv$s1 <- dat.15b.surv$surv1/dat.15b.surv$surv0
+dat.15b.surv$s2 <- dat.15b.surv$surv2/dat.15b.surv$surv0
+dat.15b.surv$d1 <- 1-dat.15b.surv$surv1/dat.15b.surv$surv0
+dat.15b.surv$d2 <- 1-dat.15b.surv$surv2/dat.15b.surv$surv0
+
+mod1 <-  glm(cbind(s1,d1) ~ species, data=dat.15b.surv, family = binomial)
+mod1
+
+dat.15b.surv$resi1 <- residuals(mod1)
+
+ggplot(dat.15b.surv, aes(x=treatment, y=resi1, colour=as.factor(exclosure))) + 
+  geom_line(position=pd) +
+  geom_point(position=pd) +
+  facet_wrap("site")
+
+# survival rate in the 16a census
+surv1.cal <- summarySE(dat.15b.surv,measurevar="resi1", groupvars=c("site","exclosure","treatment","species"))
+
+ggplot(surv1.cal, aes(x=treatment, y=resi1, colour=as.factor(exclosure))) + 
+  geom_errorbar(aes(ymin=resi1-se, ymax=resi1+se), width=.1,position=pd) +
+>>>>>>> Stashed changes
+  geom_line(position=pd) +
+  geom_point(position=pd) +
+  facet_wrap("site")
+
+<<<<<<< Updated upstream
+### end of the number of species
+
+
+###############
+### grouwth ###
+###############
+
+# 15 existing seedlings data
+head (dat.15b)
+# growth change from 15b to 16a
+dat.15b$heig1 <- dat.15b$height16a-dat.15b$height15b
+# growth change from 15b to 16b
+dat.15b$heig2 <- dat.15b$height16b-dat.15b$height15b
+
+dat.15b.heig <- aggregate(cbind(heig1,heig2)~ treatment + exclosure + site + sub.code, data=dat.15b, FUN=mean)
+heig1.cal <- summarySE(dat.15b.heig,measurevar="heig1", groupvars=c("site","exclosure","treatment"))
+heig2.cal <- summarySE(dat.15b.heig,measurevar="heig2", groupvars=c("site","exclosure","treatment"))
+
+# 16a
+ggplot(heig1.cal, aes(x=treatment, y=heig1, colour=as.factor(exclosure))) + 
+  geom_errorbar(aes(ymin=heig1-se, ymax=heig1+se), width=.1,position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd) +
+  facet_wrap("site")
+
+# 16b
+ggplot(heig2.cal, aes(x=treatment, y=heig2, colour=as.factor(exclosure))) + 
+  geom_errorbar(aes(ymin=heig2-se, ymax=heig2+se), width=.1,position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd) +
+  facet_wrap("site")
+
+
+####
+# consider the effects of species
+dat.15b.heig1 <- aggregate(cbind(heig1,heig2)~ treatment + exclosure + site + sub.code + species, data=dat.15b, FUN=mean)
+heig1.1.cal <- summarySE(dat.15b.heig1,measurevar="heig1", groupvars=c("site","exclosure","treatment"))
+heig1.2.cal <- summarySE(dat.15b.heig1,measurevar="heig2", groupvars=c("site","exclosure","treatment"))
+=======
+ggplot(dat.15b.surv, aes(x=treatment, y=resi1, colour=as.factor(exclosure))) + 
+  geom_violin() +
+  facet_wrap("site")
+
+
+# how about incorporating the growth form effect
+dat.15b.surv  <- aggregate(cbind(surv0,surv1,surv2)~ treatment + exclosure + site + sub.code + growth.form, data=dat.15b, FUN=sum)
+
+dat.15b.surv$s1 <- dat.15b.surv$surv1/dat.15b.surv$surv0
+dat.15b.surv$s2 <- dat.15b.surv$surv2/dat.15b.surv$surv0
+dat.15b.surv$d1 <- 1-dat.15b.surv$surv1/dat.15b.surv$surv0
+dat.15b.surv$d2 <- 1-dat.15b.surv$surv2/dat.15b.surv$surv0
+
+mod1 <-  glm(cbind(s1,d1) ~ growth.form, data=dat.15b.surv, family = binomial)
+mod1
+
+dat.15b.surv$resi1 <- residuals(mod1)
+ggplot(dat.15b.surv, aes(x=treatment, y=resi1, colour=as.factor(exclosure))) + 
+  geom_violin() +
+  facet_wrap("site")
+
+# survival rate in the 16a census
+surv1.cal <- summarySE(dat.15b.surv,measurevar="resi1", groupvars=c("site","exclosure","treatment","growth.form"))
+
+ggplot(surv1.cal, aes(x=treatment, y=resi1, colour=as.factor(exclosure))) + 
+  geom_errorbar(aes(ymin=resi1-se, ymax=resi1+se), width=.1,position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd) +
+  facet_wrap("site")
+
+ggplot(dat.15b.surv, aes(x=treatment, y=resi1, colour=as.factor(exclosure))) + 
+  geom_violin() +
+  facet_wrap("site")
+>>>>>>> Stashed changes
+
+# 16a
+ggplot(heig1.1.cal, aes(x=treatment, y=heig1, colour=as.factor(exclosure))) + 
+  geom_errorbar(aes(ymin=heig1-se, ymax=heig1+se), width=.1,position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd) +
+  facet_wrap("site")
+
+<<<<<<< Updated upstream
+# 16b
+ggplot(heig1.2.cal, aes(x=treatment, y=heig2, colour=as.factor(exclosure))) + 
+  geom_errorbar(aes(ymin=heig2-se, ymax=heig2+se), width=.1,position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd) +
+  facet_wrap("site")
+=======
+s1 <- glm (surv1 ~ species, data= dat.15b, family = binomial)
+dat.15b$s1.resi <- residuals(s1)
+#View(dat.15b)
+>>>>>>> Stashed changes
